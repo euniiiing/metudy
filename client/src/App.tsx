d8fadbb929fcce351fddc634a51bae168bf06f7c
euniiiing <kse8538@nate.com>
2023-12-25 23:22:35 +0900

    ThemeProvider, Global Style 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,25 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
 
 import Main from "@pages/Main";
 import Feeds from "@pages/Feeds";
-import { RecoilRoot } from "recoil";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "@/components/header/Header";
+import theme from "@/style/theme";
 
 function App() {
     return (
         <RecoilRoot>
-            <Header />
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/feeds/*" element={<Feeds />} />
-                </Routes>
-            </BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <Header />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route path="/feeds/*" element={<Feeds />} />
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider>
         </RecoilRoot>
     );
 }
