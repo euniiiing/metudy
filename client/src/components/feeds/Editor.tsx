ca9acd363f724523eb80b7afc81b308232ae57de
euniiiing <kse8538@nate.com>
2023-12-26 11:29:11 +0900

    useEditor Hooks로 분리

--- client/src/components/feeds/Editor.tsx
+++ client/src/components/feeds/Editor.tsx
@@ -1,105 +1,10 @@
-import React, { KeyboardEvent, useEffect, useRef, useState } from "react";
+import React, { KeyboardEvent } from "react";
 import styled from "styled-components";
-import ITodo from "@/api/todo/Todo";
-
-type ContentType = "text" | "sticker";
-
-interface BlockInfo {
-    type: ContentType;
-    content: string;
-}
+import useEditor from "@/hooks/useEditor";
+import { BlockInfo } from "@/hooks/useEditor";
 
 const Editor = () => {
-    const [blockInfo, setBlockInfo] = useState<BlockInfo[]>([
-        { type: "text", content: "" },
-        { type: "text", content: "" },
-        { type: "text", content: "" },
-    ]);
-    const blockContainerRef = useRef<HTMLDivElement>(null);
-    const focusBlockRef = useRef<HTMLDivElement | undefined>(undefined);
-
-    const handleFocus = () => {};
-
-    const moveToNextLine = (e: KeyboardEvent) => {
-        e.preventDefault(); // 자식 div 생성
-
-        const selection: Selection | null = window.getSelection();
-        const nodeName: string | undefined = selection?.anchorNode?.nodeName;
-        switch (nodeName) {
-            case "#text":
-                focusBlockRef.current = selection?.anchorNode?.parentNode
-                    ?.nextSibling as HTMLDivElement;
-                break;
-            case "DIV":
-                focusBlockRef.current = selection?.anchorNode?.nextSibling as HTMLDivElement;
-                break;
-            default:
-                break;
-        }
-
-        setBlockInfo((prev): BlockInfo[] => {
-            const newBlock: BlockInfo = {
-                type: "text",
-                content: "",
-            };
-            const newBlocks: BlockInfo[] = [...prev];
-            if (!focusBlockRef.current) return [];
-            newBlocks.splice(
-                parseInt(focusBlockRef.current.dataset.index as string) + 1,
-                0,
-                newBlock
-            );
-            return newBlocks;
-        });
-    };
-
-    const moveToPrevLine = (e: KeyboardEvent) => {
-        const selection: Selection | null = window.getSelection();
-        const nodeName: string | undefined = selection?.anchorNode?.nodeName;
-        switch (nodeName) {
-            case "#text":
-                if (selection?.anchorNode?.textContent) return;
-                focusBlockRef.current = selection?.anchorNode?.parentNode
-                    ?.previousSibling as HTMLDivElement;
-                break;
-            case "DIV":
-                focusBlockRef.current = selection?.anchorNode?.previousSibling as HTMLDivElement;
-                break;
-            default:
-                break;
-        }
-
-        setBlockInfo((prev): BlockInfo[] => {
-            const newBlocks: BlockInfo[] = [...prev];
-            if (!focusBlockRef.current) return [];
-            newBlocks.splice(parseInt(focusBlockRef.current.dataset.index as string) + 1, 0);
-            return newBlocks;
-        });
-    };
-
-    const handleKeyboard = (e: KeyboardEvent) => {
-        switch (e.code) {
-            case "Enter":
-                moveToNextLine(e);
-                break;
-            case "Backspace":
-                moveToPrevLine(e);
-                break;
-            default:
-                break;
-        }
-    };
-
-    useEffect(() => {
-        if (!focusBlockRef.current) return;
-        focusBlockRef.current.focus();
-    }, [blockInfo]);
-
-    useEffect(() => {
-        if (!blockContainerRef.current) return;
-        focusBlockRef.current = blockContainerRef.current.firstElementChild as HTMLDivElement;
-        focusBlockRef.current.focus();
-    }, []);
+    const { blockInfo, blockContainerRef, handleKeyboard } = useEditor();
 
     return (
         <EditorLayout ref={blockContainerRef}>
@@ -109,8 +14,7 @@ const Editor = () => {
                         data-index={idx}
                         contentEditable={true}
                         suppressContentEditableWarning={true}
-                        onFocus={handleFocus}
-                        onKeyDown={(e: any) => handleKeyboard(e)}
+                        onKeyDown={(e: KeyboardEvent) => handleKeyboard(e)}
                     >
                         {ed.content}
                     </TextBlock>
--- client/src/hooks/useEditor.ts
+++ client/src/hooks/useEditor.ts
@@ -1,8 +1,109 @@
-// interaction 로직
-const useEditor = () => {
-    const handleKeyboard = () => {};
+import React, { KeyboardEvent, useEffect, useRef, useState } from "react";
 
-    const handleFocus = () => {};
+type ContentType = "text" | "sticker";
+
+export interface BlockInfo {
+    type: ContentType;
+    content: string;
+}
+
+interface ReturnType {
+    blockInfo: BlockInfo[];
+    blockContainerRef: React.RefObject<HTMLDivElement>;
+    handleKeyboard: (e: KeyboardEvent) => void;
+}
+
+const useEditor = (): ReturnType => {
+    const [blockInfo, setBlockInfo] = useState<BlockInfo[]>([
+        { type: "text", content: "" },
+        { type: "text", content: "" },
+        { type: "text", content: "" },
+    ]);
+    const blockContainerRef = useRef<HTMLDivElement>(null);
+    const focusBlockRef = useRef<HTMLDivElement | undefined>(undefined);
+
+    const moveToNextLine = (e: KeyboardEvent): void => {
+        e.preventDefault(); // 자식 div 생성
+
+        const selection: Selection | null = window.getSelection();
+        const nodeName: string | undefined = selection?.anchorNode?.nodeName;
+        switch (nodeName) {
+            case "#text":
+                focusBlockRef.current = selection?.anchorNode?.parentNode
+                    ?.nextSibling as HTMLDivElement;
+                break;
+            case "DIV":
+                focusBlockRef.current = selection?.anchorNode?.nextSibling as HTMLDivElement;
+                break;
+            default:
+                break;
+        }
+
+        setBlockInfo((prev): BlockInfo[] => {
+            const newBlock: BlockInfo = {
+                type: "text",
+                content: "",
+            };
+            const newBlocks: BlockInfo[] = [...prev];
+            if (!focusBlockRef.current) return [];
+            newBlocks.splice(
+                parseInt(focusBlockRef.current.dataset.index as string) + 1,
+                0,
+                newBlock
+            );
+            return newBlocks;
+        });
+    };
+
+    const moveToPrevLine = (e: KeyboardEvent): void => {
+        const selection: Selection | null = window.getSelection();
+        const nodeName: string | undefined = selection?.anchorNode?.nodeName;
+        switch (nodeName) {
+            case "#text":
+                if (selection?.anchorNode?.textContent) return;
+                focusBlockRef.current = selection?.anchorNode?.parentNode
+                    ?.previousSibling as HTMLDivElement;
+                break;
+            case "DIV":
+                focusBlockRef.current = selection?.anchorNode?.previousSibling as HTMLDivElement;
+                break;
+            default:
+                break;
+        }
+
+        setBlockInfo((prev): BlockInfo[] => {
+            const newBlocks: BlockInfo[] = [...prev];
+            if (!focusBlockRef.current) return [];
+            newBlocks.splice(parseInt(focusBlockRef.current.dataset.index as string) + 1, 0);
+            return newBlocks;
+        });
+    };
+
+    const handleKeyboard = (e: KeyboardEvent): void => {
+        switch (e.code) {
+            case "Enter":
+                moveToNextLine(e);
+                break;
+            case "Backspace":
+                moveToPrevLine(e);
+                break;
+            default:
+                break;
+        }
+    };
+
+    useEffect(() => {
+        if (!focusBlockRef.current) return;
+        focusBlockRef.current.focus();
+    }, [blockInfo]);
+
+    useEffect(() => {
+        if (!blockContainerRef.current) return;
+        focusBlockRef.current = blockContainerRef.current.firstElementChild as HTMLDivElement;
+        focusBlockRef.current.focus();
+    }, []);
+
+    return { blockInfo, blockContainerRef, handleKeyboard };
 };
 
 export default useEditor;
