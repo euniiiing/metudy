a1bc750cd069b7dcf6462d7b0eb412b51d0a1a93
euniiiing <kse8538@nate.com>
2023-12-26 10:46:45 +0900

    엔터시, 텍스트 입력중 엔터시 다음 줄로 focus 이동

--- client/src/components/feeds/Editor.tsx
+++ client/src/components/feeds/Editor.tsx
@@ -1,138 +1,120 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { KeyboardEvent, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import ITodo from "@/api/todo/Todo";
 
 type ContentType = "text" | "sticker";
 
-interface ElementData {
+interface BlockInfo {
     type: ContentType;
     content: string;
 }
 
 const Editor = () => {
-    const [BlogData, setBlogData] = useState<ITodo[]>([
-        {
-            content: "",
-            progress: 0,
-            isDone: false,
-        },
-        {
-            content: "",
-            progress: 0,
-            isDone: false,
-        },
-        {
-            content: "",
-            progress: 0,
-            isDone: false,
-        },
-        {
-            content: "",
-            progress: 0,
-            isDone: false,
-        },
-    ]);
-    const caretRef = useRef<any>(undefined);
-    const [inputValue, setInputValue] = useState("");
-
-    const focusNextLine = (e: any) => {
-        if (e.code === "Enter") {
-            e.preventDefault(); // 자식 div 생성
-
-            const selection = window.getSelection();
-            const nodeName = selection?.anchorNode?.nodeName;
-
-            if (nodeName === "#text") {
-                caretRef.current = selection?.anchorNode?.parentNode;
-            } else if (nodeName === "DIV") {
-                caretRef.current = selection?.anchorNode;
-            }
-
-            setBlogData((prev) => {
-                const newText: ITodo = {
-                    content: inputValue,
-                    progress: 0,
-                    isDone: false,
-                };
-
-                const newNodes = [...prev];
-                newNodes.splice(parseInt(caretRef.current.dataset.index) + 1, 0, newText);
-                return newNodes;
-            });
-        } else if (e.code === "Backspace") {
-            const selection = window.getSelection();
-            const nodeName = selection?.anchorNode?.nodeName;
-
-            if (nodeName === "#text") {
-                caretRef.current = selection?.anchorNode?.parentNode;
-            } else if (nodeName === "DIV") {
-                caretRef.current = selection?.anchorNode;
-            }
-
-            console.dir(caretRef.current);
-
-            setBlogData((prev) => {
-                const newNodes = [...prev];
-                newNodes.splice(parseInt(caretRef.current.dataset.index) + 1, 1);
-                return newNodes;
-            });
-        } else {
-            console.log(e.code);
-            return;
-        }
-    };
-
-    useEffect(() => {
-        if (!caretRef.current) return;
-        caretRef.current = caretRef.current.nextSibling;
-        // caretRef.current = caretRef.current.previousSibling;
-        caretRef.current.focus();
-    }, [BlogData]);
-
-    const changeCaret = () => {};
-
-    const makeDiarySticker = (e: any) => {
-        const newSticker: ITodo = {
-            content: e.target.textContent,
-            progress: 0,
-            isDone: false,
-        };
-        setBlogData((prev) => [...prev, newSticker]);
-    };
-
-    const [elementData, setElementData] = useState<ElementData[]>([
+    const [blockInfo, setBlockInfo] = useState<BlockInfo[]>([
         { type: "text", content: "" },
         { type: "text", content: "" },
         { type: "text", content: "" },
     ]);
     const blockContainerRef = useRef<HTMLDivElement>(null);
-    const focusElementRef = useRef<HTMLDivElement | undefined>(undefined);
+    const focusBlockRef = useRef<HTMLDivElement | undefined>(undefined);
+
+    const handleFocus = () => {};
+
+    const moveToNextLine = (e: KeyboardEvent) => {
+        e.preventDefault(); // 자식 div 생성
+
+        const selection: Selection | null = window.getSelection();
+        const nodeName: string | undefined = selection?.anchorNode?.nodeName;
+        console.log(nodeName);
+        switch (nodeName) {
+            case "#text":
+                focusBlockRef.current = selection?.anchorNode?.parentNode
+                    ?.nextSibling as HTMLDivElement;
+                break;
+            case "DIV":
+                focusBlockRef.current = selection?.anchorNode?.nextSibling as HTMLDivElement;
+                break;
+            default:
+                break;
+        }
+
+        setBlockInfo((prev): BlockInfo[] => {
+            const newBlock: BlockInfo = {
+                type: "text",
+                content: "",
+            };
+            const newBlocks: BlockInfo[] = [...prev];
+            if (!focusBlockRef.current) return [];
+            newBlocks.splice(
+                parseInt(focusBlockRef.current.dataset.index as string) + 1,
+                0,
+                newBlock
+            );
+            return newBlocks;
+        });
+    };
+
+    const moveToPrevLine = (e: KeyboardEvent) => {
+        const selection = window.getSelection();
+        const nodeName = selection?.anchorNode?.nodeName;
+        // switch (nodeName) {
+        //     case "#text":
+        //         focusBlockRef.current = selection?.anchorNode?.parentNode;
+        //         break;
+        //     case "DIV":
+        //         focusBlockRef.current = selection?.anchorNode;
+        //         break;
+        //     default:
+        //         break;
+        // }
+
+        // setBlogData((prev) => {
+        //     const newNodes = [...prev];
+        //     newNodes.splice(parseInt(caretRef.current.dataset.index) + 1, 1);
+        //     return newNodes;
+        // });
+    };
+
+    const handleKeyboard = (e: KeyboardEvent) => {
+        switch (e.code) {
+            case "Enter":
+                moveToNextLine(e);
+                return;
+            case "Backspace":
+                moveToPrevLine(e);
+                return;
+            default:
+                return;
+        }
+    };
+
+    useEffect(() => {
+        console.log(focusBlockRef.current);
+        if (!focusBlockRef.current) return;
+        focusBlockRef.current.focus();
+    }, [blockInfo]);
 
     useEffect(() => {
         if (!blockContainerRef.current) return;
-        focusElementRef.current = blockContainerRef.current.firstElementChild as HTMLDivElement;
-        focusElementRef.current.focus();
+        focusBlockRef.current = blockContainerRef.current.firstElementChild as HTMLDivElement;
+        focusBlockRef.current.focus();
     }, []);
 
     return (
-        <EditorLayout>
-            <BlockEditorLayout ref={blockContainerRef}>
-                {elementData.map((ed: ElementData, idx: number) => {
-                    return (
-                        <TextBlock
-                            data-index={idx}
-                            contentEditable={true}
-                            draggable={true}
-                            suppressContentEditableWarning={true}
-                            onKeyDown={(e: any) => focusNextLine(e)}
-                            onFocus={changeCaret}
-                            onChange={(e: any) => setInputValue(e.target.value)}
-                        >
-                            {ed.content}
-                        </TextBlock>
-                    );
-                })}
-            </BlockEditorLayout>
+        <EditorLayout ref={blockContainerRef}>
+            {blockInfo.map((ed: BlockInfo, idx: number) => {
+                return (
+                    <TextBlock
+                        data-index={idx}
+                        contentEditable={true}
+                        suppressContentEditableWarning={true}
+                        onFocus={handleFocus}
+                        onKeyDown={(e: any) => handleKeyboard(e)}
+                    >
+                        {ed.content}
+                    </TextBlock>
+                );
+            })}
         </EditorLayout>
     );
 };
@@ -141,11 +123,7 @@ const EditorLayout = styled.div`
     border: 1px solid #e3e3e3;
     border-radius: 1em;
     width: 100%;
-`;
-
-const BlockEditorLayout = styled.div`
-    width: 100%;
-    margin-bottom: 0.8em;
+    padding-bottom: 0.8em;
 
     *:focus {
         outline: none;
