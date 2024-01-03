f41a7c33ac74d6ac166975c4e424906671b691c2
euniiiing <kse8538@nate.com>
2024-01-03 10:04:37 +0900

    query client 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import Main from "@pages/Main";
 import Todo from "@/pages/Todo";
@@ -11,20 +12,24 @@ import Header from "@/components/Header";
 import theme from "@/style/theme";
 
 function App() {
+    const [queryClient] = useState(() => new QueryClient());
+
     return (
         <RecoilRoot>
-            <ThemeProvider theme={theme}>
-                <MainLayout>
-                    <BrowserRouter>
-                        <Header />
-                        <Routes>
-                            <Route path="/" element={<Main />} />
-                            <Route path="/todo/*" element={<Todo />} />
-                            <Route path="/feeds/*" element={<Feeds />} />
-                        </Routes>
-                    </BrowserRouter>
-                </MainLayout>
-            </ThemeProvider>
+            <QueryClientProvider client={queryClient}>
+                <ThemeProvider theme={theme}>
+                    <MainLayout>
+                        <BrowserRouter>
+                            <Header />
+                            <Routes>
+                                <Route path="/" element={<Main />} />
+                                <Route path="/todo/*" element={<Todo />} />
+                                <Route path="/feeds/*" element={<Feeds />} />
+                            </Routes>
+                        </BrowserRouter>
+                    </MainLayout>
+                </ThemeProvider>
+            </QueryClientProvider>
         </RecoilRoot>
     );
 }
