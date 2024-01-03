6d3dd303f409b3003646dea1e6ee073f02018037
euniiiing <kse8538@nate.com>
2024-01-03 12:10:11 +0900

    getmytodo query key 변경

--- client/src/queries/useGetMyTodo.ts
+++ client/src/queries/useGetMyTodo.ts
@@ -1,12 +1,11 @@
 import { useQuery } from "react-query";
 import { getMyTodo } from "@/api/todo/get-my-todo";
 
-const QUERY_KEY = "get-my-todolist";
-
 const useGetMyTodo = () => {
     const { data, isLoading } = useQuery({
-        queryKey: [QUERY_KEY],
+        queryKey: ["myTodo"],
         queryFn: getMyTodo,
+        cacheTime: Infinity,
     });
     return { data, isLoading };
 };
